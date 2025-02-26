d33c82dd39451fb9862c4a212b64fb1944789c54
sirikupmo <sirikupmo@loxley.co.th>
2025-02-26 13:02:54 +0700

    Enhance GitHub Actions workflow to include Next.js export step and tidy up next.config.mjs formatting

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,10 @@
 const isProd = process.env.NODE_ENV === 'production';
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export', // แปลงเป็น Static HTML สำหรับ GitHub Pages
-    images: { unoptimized: true }, // ปิด Image Optimization
-    assetPrefix: isProd ? '/demo-fuels-price/' : '',
-    basePath: isProd ? '/demo-fuels-price' : '',
-  };
+  output: 'export', // แปลงเป็น Static HTML สำหรับ GitHub Pages
+  images: { unoptimized: true }, // ปิด Image Optimization
+  assetPrefix: isProd ? '/demo-fuels-price/' : '',
+  basePath: isProd ? '/demo-fuels-price' : '',
+};
 
 export default nextConfig;
